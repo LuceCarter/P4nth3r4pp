79248b7347dd40190e763c8602bac13a058a5a0c
Luce Carter <luce@lucyscarter.co.uk>
2020-09-25 18:12:45 +0100

    Added in the headline and made a start on the SVG banner with Paths

--- P4nth3r4pp/App.xaml.cs
+++ P4nth3r4pp/App.xaml.cs
@@ -3,12 +3,14 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using P4nth3r4pp.Views;
 
+[assembly: ExportFont("WorkSans.ttf") ]
 namespace P4nth3r4pp
 {
     public partial class App : Application
     {
         public App()
         {
+            Device.SetFlags(new string[] { "Shapes_Experimental" });
             InitializeComponent();
 
             MainPage = new HomePage();
